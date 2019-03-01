3fdbf4fffb9f0671972d3e4e3c648ab68ff84a12
Niña Kristel M. Cheng <nkmcheng@live.com>
2019-03-01 11:53:24 +0800

    added images for vocabulary

---
+++ modules/Vocabulary/images.js
@@ -0,0 +1,28 @@
+const images = {
+  1: require("./img/voc1.png"),
+  2: require("./img/voc2.png"),
+  3: require("./img/voc3.png"),
+  4: require("./img/voc4.png"),
+  5: require("./img/voc5.png"),
+  6: require("./img/voc6.png"),
+  7: require("./img/voc7.png"),
+  8: require("./img/voc8.png"),
+  9: require("./img/voc9.png"),
+  10: require("./img/voc10.png"),
+  11: require("./img/voc11.png"),
+  12: require("./img/voc12.png"),
+  14: require("./img/voc14.png"),
+  15: require("./img/voc15.png"),
+  16: require("./img/voc16.png"),
+  17: require("./img/voc17.png"),
+  18: require("./img/voc18.png"),
+  19: require("./img/voc19.png"),
+  20: require("./img/voc20.png"),
+  21: require("./img/voc21.png"),
+  22: require("./img/voc22.png"),
+  23: require("./img/voc23.png"),
+  24: require("./img/voc24.png"),
+  25: require("./img/voc25.png")
+};
+
+export default images;
